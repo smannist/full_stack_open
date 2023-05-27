8c965e62245f93f43487da9ac5e4104c493a267a
Sami Männistö <sami.mannisto1@gmail.com>
2023-05-27 11:53:48 +0300

    Add 1.2

--- part_1/kurssitiedot/src/App.js
+++ part_1/kurssitiedot/src/App.js
@@ -11,15 +11,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </div>
   )
 }
@@ -31,6 +25,16 @@ const Total = (props) => {
         Number of exercises {props.exTtlAmount}
       </p>
     </div>
+  )
+}
+
+const Part = (props) => {
+  const paragraph = { part: props.part, exercises: props.exercises }
+
+  return (
+    <p>
+      {paragraph.part} {paragraph.exercises}
+    </p>
   )
 }
 
