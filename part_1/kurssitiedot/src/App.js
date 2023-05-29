5acd9ea2216a9e7660e0a03b1fedab178f2a3e74
Sami Männistö <sami.mannisto1@gmail.com>
2023-05-29 11:55:49 +0300

    1.5 single object

--- part_1/kurssitiedot/src/App.js
+++ part_1/kurssitiedot/src/App.js
@@ -1,8 +1,8 @@
-const Header = (props) => {
+const Header = ({ course }) => {
   return (
     <div>
       <h1>
-        {props.course}
+        {course.name}
       </h1>
     </div>
   )
@@ -39,31 +39,33 @@ const Part = (props) => {
       {paragraph.part} {paragraph.exercises}
     </p>
   )
+
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </>
   )
 }
