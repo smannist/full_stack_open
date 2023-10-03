0d0a20c927dcffa9ce0d4e41363ba7cbbb1d331a
Sami Männistö <sami.mannisto1@gmail.com>
2023-10-03 14:05:08 +0300

    Ex 1.7

--- part_1/unicafe/src/components/statistics.jsx
+++ part_1/unicafe/src/components/statistics.jsx
@@ -1,11 +1,28 @@
 import React from "react";
 
-const Statistics = ({ valueText1, valueText2, valueText3, value1, value2, value3 }) => (
-  <p>
-    {valueText1} {value1}
-    <br></br>{valueText2} {value2}
-    <br></br>{valueText3} {value3}
-  </p>
-)
+const Statistics = ({ valueText1, valueText2, valueText3, value1, value2, value3 }) => {
+  const total = value1 + value2 + value3;
+  const average = total === 0 ? 0 : (value1 * 1 + value2 * 0 + value3 * -1) / total;
+  const positivePercentage = total === 0 ? 0 : (value1 / total) * 100;
+
+  return (
+    <div>
+      <p>
+        {valueText1}: {value1}
+        <br />
+        {valueText2}: {value2}
+        <br />
+        {valueText3}: {value3}
+        <br />
+        Total: {total}
+        <br />
+        Average: {average}
+        <br />
+        Positive: {positivePercentage} %
+      </p>
+    </div>
+  );
+
+};
 
 export default Statistics;
