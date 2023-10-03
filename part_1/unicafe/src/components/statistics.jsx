03b1f960ec5b522a8e03d4dcd16566c525fc0d16
Sami Männistö <sami.mannisto1@gmail.com>
2023-10-03 14:13:43 +0300

    ex 1.9 conditional rendering

--- part_1/unicafe/src/components/statistics.jsx
+++ part_1/unicafe/src/components/statistics.jsx
@@ -5,23 +5,31 @@ const Statistics = ({ valueText1, valueText2, valueText3, value1, value2, value3
   const average = total === 0 ? 0 : (value1 * 1 + value2 * 0 + value3 * -1) / total;
   const positivePercentage = total === 0 ? 0 : (value1 / total) * 100;
 
+  if (total > 0) {
+    return (
+      <div>
+        <p>
+          {valueText1}: {value1}
+          <br />
+          {valueText2}: {value2}
+          <br />
+          {valueText3}: {value3}
+          <br />
+          Total: {total}
+          <br />
+          Average: {average}
+          <br />
+          Positive: {positivePercentage} %
+        </p>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <p>
-        {valueText1}: {value1}
-        <br />
-        {valueText2}: {value2}
-        <br />
-        {valueText3}: {value3}
-        <br />
-        Total: {total}
-        <br />
-        Average: {average}
-        <br />
-        Positive: {positivePercentage} %
-      </p>
-    </div>
-  );
+    <p>
+      There is not any feedback yet
+    </p>
+  )
 
 };
 
