9934feb11bb770fc9b1df438276cef56be1edf6a
Sami Männistö <sami.mannisto1@gmail.com>
2023-12-24 13:54:05 +0200

    e.x. 2.6

---
+++ part_2/phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Person from "./components/person";
+
+const App = (props) => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+
+    const namesObject = {
+      id: persons.length + 1,
+      name: newName,
+    };
+
+    setPersons(persons.concat(namesObject));
+    setNewName("");
+  };
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    console.log(persons);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name:
+          <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map(person => (
+          <Person key={person.id} name={person.name} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
