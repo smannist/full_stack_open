d30375ca68ac960527f820e6b74171245f08c044
Sami Männistö <sami.mannisto1@gmail.com>
2023-12-25 21:53:51 +0200

    e.x. 2.11, fetch default state from server

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,18 +1,15 @@
 import { useState } from "react";
 import Persons from "./components/persons";
 import PhonebookForm from "./components/phonebook_form";
+import getDefaultPersons from "./hooks";
 
 const App = () => {
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+  const [persons, setPersons] = useState([]);
 
-  const [persons, setPersons] = useState([
-    { id: 0, name: "Arto Hellas", number: "040-123456" },
-    { id: 1, name: "Ada Lovelace", number: "39-44-5323523" },
-    { id: 2, name: "Dan Abramov", number: "12-43-234345" },
-    { id: 3, name: "Mary Poppendieck", number: "39-23-6423122" },
-  ]);
+  getDefaultPersons(setPersons);
 
   const addPerson = (event) => {
     event.preventDefault();
