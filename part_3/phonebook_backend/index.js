970a482044897eefe75f0daa58d36863580bcf5c
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-08 06:11:06 +0200

    e.x 3.1 persons endpoint, node scripts

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -1,1 +1,36 @@
-// nothing here yet!
+const express = require("express");
+const app = express();
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendick",
+    number: "39-23-6423122",
+  },
+];
+
+app.get(process.env.PERSONS_API_URL, (request, response) => {
+  response.json(persons);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
