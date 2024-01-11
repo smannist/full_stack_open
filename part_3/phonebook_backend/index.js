55c7a4007e33d4c0c3219463d216c61d3a2d96e2
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-11 21:11:59 +0200

    Deleted hardcoded persons array, since it is no longer necessary

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -15,29 +15,6 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendick",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/api/persons", (request, response) => {
   Person
     .find({})
@@ -95,7 +72,7 @@ app.delete("/api/persons/:id", (request, response) => {
     })
     .catch(error => {
       console.log(error);
-      
+
     })
 });
 
