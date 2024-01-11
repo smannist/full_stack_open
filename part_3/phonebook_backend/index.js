48f49d920857f2bb8663f62748b74cb549a45246
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-11 20:48:04 +0200

    Error handling for singular person search

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -45,11 +45,18 @@ app.get("/api/persons", (request, response) => {
 });
 
 app.get("/api/persons/:id", (request, response) => {
-  Person
-    .findById(request.params.id)
-    .then(person => {
-      response.json(person);
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
     })
+    .catch((error) => {
+      console.log(error);
+      response.status(400).send({ error: 'Malformatted id' })
+    });
 });
 
 app.post("/api/persons", (request, response) => {
@@ -72,7 +79,6 @@ app.post("/api/persons", (request, response) => {
   person.save().then((savedPerson) => {
     response.json(savedPerson);
   });
-
 });
 
 app.delete("/api/persons/:id", (request, response) => {
