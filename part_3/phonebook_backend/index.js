a1ae8a1cfc8ddaa3f15e952c18deb7f05468f559
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-11 21:31:32 +0200

    e.x. 3.16, error handling into middleware

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -2,7 +2,10 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const Person = require("./models/person");
+const { unknownEndpoint, errorHandler } = require("./middleware/person");
 require("dotenv").config();
+
+const PORT = process.env.PORT || 3001;
 
 morgan.token("body", (request, response) => JSON.stringify(request.body));
 
@@ -23,7 +26,7 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response, next) => {
   Person
     .findById(request.params.id)
     .then((person) => {
@@ -33,10 +36,7 @@ app.get("/api/persons/:id", (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: 'Malformatted id' })
-    });
+    .catch(error => next(error));
 });
 
 app.post("/api/persons", (request, response) => {
@@ -64,16 +64,13 @@ app.post("/api/persons", (request, response) => {
 
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person
     .findByIdAndDelete(request.params.id)
     .then(result => {
       response.status(204).end();
     })
-    .catch(error => {
-      console.log(error);
-
-    })
+    .catch(error => next(error));
 });
 
 app.get("/info", (request, response) => {
@@ -84,8 +81,9 @@ app.get("/info", (request, response) => {
   );
 });
 
-const PORT = process.env.PORT || 3001;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
---
+++ part_3/phonebook_backend/middleware/person.js
@@ -0,0 +1,22 @@
+const unknownEndpoint = (request, response) => {
+  response
+    .status(404)
+    .send({ error: 'Unknown endpoint' });
+}
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return response
+            .status(400)
+            .send({ error: 'Malformatted id' });
+  }
+
+  next(error);
+}
+
+module.exports = {
+  unknownEndpoint,
+  errorHandler,
+};
