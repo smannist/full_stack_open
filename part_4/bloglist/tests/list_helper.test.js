4e10635281f4760575791baf588e329874a2de97
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-12 15:15:40 +0200

    e.x. 4.4, more testing

--- part_4/bloglist/tests/list_helper.test.js
+++ part_4/bloglist/tests/list_helper.test.js
@@ -1,8 +1,25 @@
 const listHelper = require("../utils/list_helper");
+const mockData = require("./mock_data/blog");
 
 test("dummy returns one", () => {
   const blogs = [];
-
   const result = listHelper.dummy(blogs);
   expect(result).toBe(1);
+});
+
+describe("total likes", () => {
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
+  });
+
+  test("when list has only one blog equals the likes of that", () => {
+    const result = listHelper.totalLikes(mockData.listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  test("of a bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(mockData.blogs);
+    expect(result).toBe(36);
+  });
 });
