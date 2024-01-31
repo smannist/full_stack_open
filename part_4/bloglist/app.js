e5420ceb2fa890a26c819aa40341593b13a0e551
Sami Männistö <sami.mannisto1@gmail.com>
2024-01-31 17:22:42 +0200

    POST and GET new user

--- part_4/bloglist/app.js
+++ part_4/bloglist/app.js
@@ -3,6 +3,7 @@ const morgan = require("morgan");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const blogsRouter = require("./controllers/blogs");
+const usersRouter = require('./controllers/users')
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 const dotenv = require("dotenv");
@@ -24,5 +25,6 @@ app.use(express.json());
 app.use(logger.createMorganMiddleware());
 
 app.use("/api/blogs", blogsRouter);
+app.use('/api/users', usersRouter)
 
 module.exports = app;
