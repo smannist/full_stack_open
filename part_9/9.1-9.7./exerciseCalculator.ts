00d2104e39138d31c38d4b57149064a27b052b32
Sami Männistö <sami.mannisto1@gmail.com>
2024-02-03 18:21:19 +0200

    e.x. 9.2, exercise calculator

---
+++ part_9/9.1-9.7./exerciseCalculator.ts
@@ -0,0 +1,51 @@
+interface ExerciseInfo {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+const calculateExercises = (
+  hours: Array<number>,
+  target: number
+): ExerciseInfo => {
+  let rating;
+  let ratingDescription;
+
+  const periodLength = hours.length;
+  const trainingDays = hours.filter((hour) => hour > 0).length;
+  const average =
+    hours.reduce((total, hours) => total + hours, 0) / hours.length;
+  const success = average >= target;
+
+  if (average >= target) {
+    rating = 3;
+    ratingDescription = "Excellent! Keep up the good work!";
+  } else if (average >= target / 2) {
+    rating = 2;
+    ratingDescription = "Decent, but you can do better.";
+  } else {
+    rating = 1;
+    ratingDescription = "Try harder!";
+  }
+
+  const ExerciseInfo = {
+    periodLength,
+    trainingDays,
+    success,
+    rating,
+    ratingDescription,
+    target,
+    average,
+  };
+
+  return ExerciseInfo;
+};
+
+const trainingDays = [3, 0, 2, 4.5, 0, 3, 1];
+const target = 2;
+
+console.log(calculateExercises(trainingDays, target));
