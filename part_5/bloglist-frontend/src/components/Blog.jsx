47a979e63521f580a50ecb5bf13f688dba51da7f
Sami Männistö <sami.mannisto1@gmail.com>
2024-02-28 08:21:46 +0200

    e.x. 5.7, show blog details on click

--- part_5/bloglist-frontend/src/components/Blog.jsx
+++ part_5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,34 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-);
+import "../App.css";
+import React, { useState } from "react";
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const handleVisibilityChange = () => {
+    setVisible(!visible);
+  };
+
+  if (visible) {
+    return (
+      <div className="blog">
+        {blog.title} {blog.author}
+        <button onClick={handleVisibilityChange}>Hide</button>
+        <br></br>
+        <strong>Url:</strong> {blog.url}
+        <br></br>
+        <strong>Likes:</strong> {blog.likes} <button>Like</button>
+        <br></br>
+        <strong>Added by:</strong> {blog.user.username}
+      </div>
+    );
+  }
+
+  return (
+    <div className="blog">
+      {blog.title} {blog.author}
+      <button onClick={handleVisibilityChange}>View</button>
+    </div>
+  );
+};
 
 export default Blog;
