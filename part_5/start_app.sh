f51ef826ed129ccd5d41337deb7c813b9dea5b6f
Sami Männistö <sami.mannisto1@gmail.com>
2024-03-15 16:54:14 +0200

    Small fix for script

--- part_5/start_app.sh
+++ part_5/start_app.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
-# starts front and backend for blog app and opens front in the browser after a few seconds
+# starts front and backend for blog app in separate terminals
 
 cd ../part_4/bloglist/
 gnome-terminal -- npm run dev
 
 cd ../../part_5/bloglist-frontend/
-npm run dev &
-
-sleep 3
-xdg-open http://localhost:5175/
+gnome-terminal -- npm run dev &
