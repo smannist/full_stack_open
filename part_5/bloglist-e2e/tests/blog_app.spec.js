ab96e9f8994b940d8a578dbef51c2827f93aa1f4
Sami Männistö <sami.mannisto1@gmail.com>
2024-03-16 15:00:57 +0200

    E.x. 5.21, test removing a blog

--- part_5/bloglist-e2e/tests/blog_app.spec.js
+++ part_5/bloglist-e2e/tests/blog_app.spec.js
@@ -30,9 +30,7 @@ describe("Blog app", () => {
       await page.getByTestId("password").fill("Testing");
       await page.getByRole("button", { name: "login" }).click();
 
-      await expect(
-        page.getByText("Logged in as McTesterino")
-      ).toBeVisible();
+      await expect(page.getByText("Logged in as McTesterino")).toBeVisible();
     });
 
     test("fails with wrong credentials", async ({ page }) => {
@@ -40,9 +38,7 @@ describe("Blog app", () => {
       await page.getByTestId("password").fill("2");
       await page.getByRole("button", { name: "login" }).click();
 
-      await expect(
-        page.getByText("Incorrect login credentials")
-      ).toBeVisible();
+      await expect(page.getByText("Incorrect login credentials")).toBeVisible();
     });
 
     describe("When logged in", () => {
@@ -60,8 +56,8 @@ describe("Blog app", () => {
         await page.getByRole("button", { name: "Create" }).click();
 
         await expect(
-          page.getByText(mockBlog.title + " " + mockBlog.author)
-        ).toBeVisible();
+          page.getByText(`${mockBlog.title} ${mockBlog.author}`)
+        ).not.toBeVisible();
       });
 
       describe("When a new blog has been created", () => {
@@ -71,14 +67,32 @@ describe("Blog app", () => {
           await page.getByTestId("author").fill(mockBlog.author);
           await page.getByTestId("url").fill(mockBlog.url);
           await page.getByRole("button", { name: "Create" }).click();
+          await page.getByRole("button", { name: "View" }).click();
         });
 
-        test("blog can be liked", async ({ page }) => {
-          await page.getByRole("button", { name: "View" }).click();
+        test("a blog can be liked", async ({ page }) => {
           await expect(page.getByText("Likes: 0")).toBeVisible();
-
           await page.getByRole("button", { name: "Like" }).click();
           await expect(page.getByText("Likes: 1")).toBeVisible();
+        });
+
+        test("a blog can be removed", async ({ page }) => {
+          page.on("dialog", async (dialog) => {
+            expect(dialog.message()).toEqual(
+              `Remove blog ${mockBlog.title} by ${mockBlog.author}?`
+            );
+            await dialog.accept();
+          });
+
+          await page.getByRole("button", { name: "Remove" }).click();
+
+          await expect(
+            page.getByText(`${mockBlog.title} ${mockBlog.author}`)
+          ).not.toBeVisible();
+
+          await expect(
+            page.getByText(`Blog "${mockBlog.title}" removed!`)
+          ).toBeVisible();
         });
       });
     });
