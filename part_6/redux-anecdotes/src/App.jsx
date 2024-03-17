418c3ec0679cddfd9ae9d7c6522c2bf30edb0072
Sami Männistö <sami.mannisto1@gmail.com>
2024-03-17 21:37:53 +0200

    E.x. 6.7, anecdote creation into seperate component

--- part_6/redux-anecdotes/src/App.jsx
+++ part_6/redux-anecdotes/src/App.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
-import { addVote, createAnecdote } from "./reducers/anecdoteReducer";
+import { addVote } from "./reducers/anecdoteReducer";
+import AnecdoteForm from "./components/AnecdoteForm";
 
 const App = () => {
   const anecdotes = useSelector((state) =>
@@ -11,13 +12,6 @@ const App = () => {
   const vote = (id) => {
     console.log("vote", id);
     dispatch(addVote(id));
-  };
-
-  const addAnecdote = (event) => {
-    event.preventDefault();
-    const content = event.target.anecdote.value;
-    event.target.anecdote.value = "";
-    dispatch(createAnecdote(content));
   };
 
   return (
@@ -33,12 +27,7 @@ const App = () => {
         </div>
       ))}
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div>
-          <input name="anecdote" />
-        </div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm />
     </div>
   );
 };
