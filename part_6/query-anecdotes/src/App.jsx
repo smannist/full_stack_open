52965c80df61fe30d9988faf8a30588cc8df8da4
Sami Männistö <sami.mannisto1@gmail.com>
2024-03-19 22:47:54 +0200

    E.x. 6.20, react query anecdotes + display error msg

--- part_6/query-anecdotes/src/App.jsx
+++ part_6/query-anecdotes/src/App.jsx
@@ -1,40 +1,50 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { getAnecdotes } from "./requests";
 
 const App = () => {
-
   const handleVote = (anecdote) => {
-    console.log('vote')
+    console.log("vote");
+  };
+
+  const result = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: getAnecdotes,
+    retry: false,
+  });
+
+  if (result.isLoading) {
+    return <div>Loading data...</div>;
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  if (result.isError) {
+    return (
+      <div style={{ fontWeight: "bold", fontSize: "2em" }}>
+        Anecdote service is not available due server issues
+      </div>
+    );
+  }
+
+  const anecdotes = result.data;
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- part_6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ part_6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,21 +1,19 @@
 const AnecdoteForm = () => {
-
   const onCreate = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    event.preventDefault();
+    const content = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+  };
 
   return (
     <div>
       <h3>create new</h3>
       <form onSubmit={onCreate}>
-        <input name='anecdote' />
+        <input name="anecdote" />
         <button type="submit">create</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default AnecdoteForm
+export default AnecdoteForm;
