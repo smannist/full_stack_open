f650d270cd96a31134ff85ebf7ac0d653b7e99f4
Sami Männistö <sami.mannisto1@gmail.com>
2024-03-19 23:21:19 +0200

    E.x. 6.21, add new anecdote and re-render

--- part_6/query-anecdotes/src/App.jsx
+++ part_6/query-anecdotes/src/App.jsx
@@ -1,5 +1,4 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 import AnecdoteForm from "./components/AnecdoteForm";
 import Notification from "./components/Notification";
 import { getAnecdotes } from "./requests";
