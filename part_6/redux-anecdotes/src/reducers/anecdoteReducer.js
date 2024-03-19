7eedaa02e9db3307317081919a8316b731faf386
Sami Männistö <sami.mannisto1@gmail.com>
2024-03-19 17:03:55 +0200

    E.x. 6.16, initialize anecdotes with redux thunk

--- part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part_6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import anecdoteService from "../services/anecdotes";
 
 const anecdoteSlice = createSlice({
   name: "anecdotes",
@@ -26,6 +27,13 @@ const anecdoteSlice = createSlice({
     },
   },
 });
+
+export const initializeAnecdotes = () => {
+  return async (dispatch) => {
+    const anecdotes = await anecdoteService.getAll();
+    dispatch(setAnecdotes(anecdotes));
+  };
+};
 
 export const { addVote, createAnecdote, appendAnecdote, setAnecdotes } =
   anecdoteSlice.actions;
