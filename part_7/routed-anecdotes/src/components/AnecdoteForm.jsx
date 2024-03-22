7bcbb3fa879f885f89cd2c218d26e995bfc23c52
Sami Männistö <sami.mannisto1@gmail.com>
2024-03-22 17:23:20 +0200

    E.x. 7.4, useField custom hook

--- part_7/routed-anecdotes/src/components/AnecdoteForm.jsx
+++ part_7/routed-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
+import { useField } from "../hooks";
 
 const AnecdoteForm = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("content");
+  const author = useField("author");
+  const info = useField("info");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
   };
@@ -21,27 +21,15 @@ const AnecdoteForm = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
