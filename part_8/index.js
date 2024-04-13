1f21b7ae8ac2848b5a1716f9709a36d0449fc087
Sami Männistö <sami.mannisto1@gmail.com>
2024-04-13 22:29:21 +0300

    E.x. 8.4, filter books by author

--- part_8/index.js
+++ part_8/index.js
@@ -116,7 +116,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -125,7 +125,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (_, args) => {
+      if (!args.author) {
+        return books;
+      }
+      return books.filter((book) => book.author === args.author);
+    },
     allAuthors: () => authors,
   },
   Author: {
