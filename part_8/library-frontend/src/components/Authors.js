86b9d910b073464b0943a60b8f0f9d5de8d7eef1
Sami Männistö <sami.mannisto1@gmail.com>
2024-04-14 19:02:19 +0300

    E.x. 8.11, authors birthyear

--- part_8/library-frontend/src/components/Authors.js
+++ part_8/library-frontend/src/components/Authors.js
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
-import { ALL_AUTHORS } from "../queries";
+import { useMutation } from "@apollo/client";
+import { ALL_AUTHORS, UPDATE_AUTHOR } from "../queries";
 import Loading from "./Loading";
 
 const Authors = () => {
+  const [name, setName] = useState("");
+  const [born, setBorn] = useState("");
+
+  const [updateAuthor] = useMutation(UPDATE_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  });
+
   const { loading, data } = useQuery(ALL_AUTHORS);
 
   if (loading) {
     return <Loading />;
   }
+
+  const submit = async (event) => {
+    event.preventDefault();
+
+    updateAuthor({ variables: { name, born } });
+
+    setName("");
+    setBorn("");
+  };
 
   return (
     <div>
@@ -28,6 +46,24 @@ const Authors = () => {
           ))}
         </tbody>
       </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            value={born}
+            onChange={({ target }) => setBorn(Number(target.value))}
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   );
 };
--- part_8/library-frontend/src/queries.js
+++ part_8/library-frontend/src/queries.js
@@ -40,3 +40,12 @@ export const CREATE_BOOK = gql`
     }
   }
 `;
+
+export const UPDATE_AUTHOR = gql`
+  mutation updateAuthor($name: String!, $born: Int!) {
+    editAuthor(name: $name, setBornTo: $born) {
+      name
+      born
+    }
+  }
+`;
