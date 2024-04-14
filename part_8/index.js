f4bb7b945bbb87e7e9c6ca191f7dab98dbecba03
Sami Männistö <sami.mannisto1@gmail.com>
2024-04-14 12:29:02 +0300

    E.x. 8.6, addBook mutation

--- part_8/index.js
+++ part_8/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -113,12 +114,22 @@ const typeDefs = `
     bookCount: Int!
   }
 
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ) : Book
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
 `;
 
 const resolvers = {
@@ -137,6 +148,22 @@ const resolvers = {
       return root.name
         ? books.filter((book) => book.author === root.name).length
         : 0;
+    },
+  },
+  Mutation: {
+    addBook: (_, args) => {
+      const duplicateAuthor = authors.find(
+        (author) => author.name === args.author
+      );
+
+      duplicateAuthor
+        ? null
+        : (authors = authors.concat({ name: args.author, id: uuid() }));
+
+      const book = { ...args, id: uuid() };
+      books = books.concat(book);
+
+      return book;
     },
   },
 };
