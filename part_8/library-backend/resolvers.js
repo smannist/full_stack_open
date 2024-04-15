a4f7041422ff802d4e75cc15d07da55ed195ac79
Sami Männistö <sami.mannisto1@gmail.com>
2024-04-15 18:59:48 +0300

    Fix identitation

--- part_8/library-backend/resolvers.js
+++ part_8/library-backend/resolvers.js
@@ -8,66 +8,66 @@ const Author = require("./models/author");
 const resolvers = {
   Query: {
     bookCount: async () => {
-      return await Book.countDocuments({})
+      return await Book.countDocuments({});
     },
     authorCount: async () => {
-      return await Author.countDocuments({})
+      return await Author.countDocuments({});
     },
     allBooks: async (_, { genre }) => {
       return await Book
-                    .find(genre ? { genres: genre } : {})
-                    .populate("author");
+                   .find(genre ? { genres: genre } : {})
+                   .populate("author");
     },
     allAuthors: async () => {
       return Author.find({});
     },
   },
-    Author: {
-      bookCount: async (root) => {
-        return await Book.countDocuments({ author: root._id });
-      },
+  Author: {
+    bookCount: async (root) => {
+      return await Book.countDocuments({ author: root._id });
     },
-    Mutation: {
-      addBook: async (_, args) => {
-        let authorObj = await Author.findOne({ name: args.author });
+  },
+  Mutation: {
+    addBook: async (_, args) => {
+      let authorObj = await Author.findOne({ name: args.author });
 
-        if (!authorObj) {
-          try {
-            authorObj = new Author({ name: args.author });
-            await authorObj.save();
-          } catch (error) {
-            saveError(error);
-          }
-        }
-
-        const book = new Book({ ...args, author: authorObj });
-
+      if (!authorObj) {
         try {
-          await book.save();
+          authorObj = new Author({ name: args.author });
+          await authorObj.save();
         } catch (error) {
           saveError(error);
         }
+      }
 
-        return book;
-      },
-      editAuthor: async (_, { name, setBornTo }) => {
-        const author = await Author.findOne({ name: name });
+      const book = new Book({ ...args, author: authorObj });
 
-        if (!author) {
-          return null;
-        }
+      try {
+        await book.save();
+      } catch (error) {
+        saveError(error);
+      }
 
-        author.born = setBornTo;
-
-        try {
-          await author.save();
-        } catch (error) {
-          saveError(error)
-        }
-
-        return author;
-      },
+      return book;
     },
+    editAuthor: async (_, { name, setBornTo }) => {
+      const author = await Author.findOne({ name: name });
+
+      if (!author) {
+        return null;
+      }
+
+      author.born = setBornTo;
+
+      try {
+        await author.save();
+      } catch (error) {
+        saveError(error);
+      }
+
+      return author;
+    },
+  },
 };
 
 module.exports = resolvers;
