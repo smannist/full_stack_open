ad3fe5aa6629dda48223ec6d44c7fef3d63faaa2
Sami Männistö <sami.mannisto1@gmail.com>
2024-04-18 20:20:51 +0300

    E.x. 9.8, Patientor backend, step1

---
+++ part_9/patientor-backend/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/api/ping", (_, res) => {
+  res.send("pong");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
