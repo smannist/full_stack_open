e23ce56b0e70067119224ce18a4597328e723bda
Sami Männistö <sami.mannisto1@gmail.com>
2024-04-18 20:34:39 +0300

    E.x. 9.9, clone front and setup cors

--- part_9/patientor-backend/index.ts
+++ part_9/patientor-backend/index.ts
@@ -1,14 +1,16 @@
 import express from "express";
 
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/api/ping", (_, res) => {
   res.send("pong");
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
