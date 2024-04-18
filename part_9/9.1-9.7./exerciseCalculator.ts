edffff42d7216402b0d8dbe3ce852a37c4b29005
Sami Männistö <sami.mannisto1@gmail.com>
2024-04-18 19:12:05 +0300

    E.x. 9.7, WebExercises

--- part_9/9.1-9.7./exerciseCalculator.ts
+++ part_9/9.1-9.7./exerciseCalculator.ts
@@ -10,7 +10,7 @@ interface ExerciseInfo {
   average: number;
 }
 
-const calculateExercises = (
+export const calculateExercises = (
   target: number,
   hours: number[],
 ): ExerciseInfo => {
--- part_9/9.1-9.7./index.ts
+++ part_9/9.1-9.7./index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import { calculateBmi } from "./bmiCalculator";
+import { calculateExercises } from "./exerciseCalculator";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/hello", (_, res) => {
   res.send("Hello Full Stack!");
@@ -19,8 +22,34 @@ app.get("/bmi", (req, res) => {
   return res.json({ weight: weight, height: height, bmi: bmi });
 });
 
+app.post("/exercises", (req, res) => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { daily_exercises, target } = req.body;
+
+  if (!daily_exercises || !target) {
+    return res.status(400).send({ error: "parameters missing" });
+  }
+
+  if (
+    !Array.isArray(daily_exercises) ||
+    daily_exercises.some(isNaN) ||
+    isNaN(Number(target))
+  ) {
+    return res.status(400).send({ error: "malformatted parameters" });
+  }
+
+  const result = calculateExercises(
+    Number(target),
+    daily_exercises.map((hour: number) => Number(hour))
+  );
+
+  return res.json(result);
+});
+
 const PORT = 3003;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+app.use(express.json());
