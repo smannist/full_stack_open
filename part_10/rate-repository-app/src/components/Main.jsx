9d56a5ca54924f8bb6f6868134c2fa4fefb8a8c3
Sami Männistö <sami.mannisto1@gmail.com>
2024-05-24 20:06:17 +0300

    Add NativeRouter

--- part_10/rate-repository-app/src/components/Main.jsx
+++ part_10/rate-repository-app/src/components/Main.jsx
@@ -1,4 +1,5 @@
 import { View } from "react-native";
+import { Route, Routes, Navigate } from "react-router-native";
 
 import RepositoryList from "./RepositoryList";
 import AppBar from "./AppBar/AppBar";
@@ -7,7 +8,10 @@ const Main = () => {
   return (
     <View style={{ flexGrow: 1, flexShrink: 1, backgroundColor: "#e1e4e8" }}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
