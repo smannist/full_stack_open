2f195baa5caf07752a232fc478d9754cc45f9a67
Sami Männistö <sami.mannisto1@gmail.com>
2024-05-24 15:15:44 +0300

    Add theme + custom Text component

--- part_10/rate-repository-app/src/components/AppBarTab.jsx
+++ part_10/rate-repository-app/src/components/AppBarTab.jsx
@@ -1,4 +1,6 @@
-import { Text, Pressable, StyleSheet } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
+
+import Text from "./Text";
 
 const styles = StyleSheet.create({
   text: {
