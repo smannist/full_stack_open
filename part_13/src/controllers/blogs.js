fcaa6237c00a5e8baa7d53a6b843f7ae48527bdf
Sami Männistö <sami.mannisto1@gmail.com>
2024-06-02 14:53:24 +0300

    E.x. 13.6 - Update blog likes

--- part_13/src/controllers/blogs.js
+++ part_13/src/controllers/blogs.js
@@ -10,6 +10,19 @@ router.post("/", async (req, res) => {
   const blog = await Blog.create(req.body);
 });
 
+router.put("/:id", async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+
+  if (blog) {
+    blog.likes = req.body.likes;
+    await blog.save();
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+
+});
+
 router.delete("/:id", async (req, res) => {
   const blog = await Blog.destroy({
     where: {
