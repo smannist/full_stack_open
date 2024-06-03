aecfec0a0b2e71b2d61ba080a0edbde67cd06884
Sami Männistö <sami.mannisto1@gmail.com>
2024-06-03 09:23:58 +0300

    E.x. 13.16 - authors endpoint + bonus

---
+++ part_13/src/controllers/authors.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+
+const { Blog } = require("../models");
+
+const { sequelize } = require("../utils/db");
+
+router.get("/", async (_, res) => {
+  const authors = await Blog.findAll({
+    attributes: [
+      "author",
+      [
+        sequelize.fn("COUNT", sequelize.col("id")), "articles"
+      ],
+      [
+        sequelize.fn("SUM", sequelize.col("likes")), "likes"
+      ],
+    ],
+    group: "author",
+    order: [
+      ["likes", "DESC"],
+    ],
+  });
+
+  res.json(authors);
+});
+
+module.exports = router;
--- part_13/src/index.js
+++ part_13/src/index.js
@@ -13,6 +13,7 @@ const errorHandler = require("./middleware/errorHandler");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
 const loginRouter = require("./controllers/login");
+const authorsRouter = require("./controllers/authors");
 
 app.use(express.json());
 app.use(cors());
@@ -20,6 +21,7 @@ app.use(cors());
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/login", loginRouter);
+app.use("/api/authors", authorsRouter);
 
 app.use(errorHandler);
 
