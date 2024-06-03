f9a2094b884418bff4dfd01cf701a3b3b8c4ca42
Sami Männistö <sami.mannisto1@gmail.com>
2024-06-03 11:18:35 +0300

    E.x. 13.17 - Migrations

--- part_13/src/utils/db.js
+++ part_13/src/utils/db.js
@@ -1,4 +1,5 @@
 const Sequelize = require("sequelize");
+const { Umzug, SequelizeStorage } = require("umzug");
 
 const sequelize = new Sequelize({
   dialect: "postgres",
@@ -11,6 +12,7 @@ const sequelize = new Sequelize({
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log("connected to the database");
   } catch (err) {
     console.log("failed to connect to the database");
@@ -20,4 +22,27 @@ const connectToDatabase = async () => {
   return null;
 };
 
-module.exports = { connectToDatabase, sequelize };
+const migrationConf = {
+  migrations: {
+    glob: "src/migrations/*.js",
+  },
+  storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+  context: sequelize.getQueryInterface(),
+  logger: console,
+};
+
+const runMigrations = async () => {
+  const migrator = new Umzug(migrationConf);
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
+const rollbackMigration = async () => {
+  await sequelize.authenticate();
+  const migrator = new Umzug(migrationConf);
+  await migrator.down();
+};
+
+module.exports = { connectToDatabase, sequelize, rollbackMigration };
