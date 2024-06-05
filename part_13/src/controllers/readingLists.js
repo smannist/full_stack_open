bf68e394d4b9702e811b84d9c05729c718733f03
Sami Männistö <sami.mannisto1@gmail.com>
2024-06-05 15:00:55 +0300

    E.x. 13.22 - Mark as read endpoint

--- part_13/src/controllers/readingLists.js
+++ part_13/src/controllers/readingLists.js
@@ -1,11 +1,25 @@
 const router = require("express").Router();
 
-const { ReadingList } = require("../models");
+const { ReadingList, User } = require("../models");
+const tokenExtractor = require("../middleware/tokenExtractor");
 
 router.post("/", async (req, res) => {
   const addToReadingList = await ReadingList.create(req.body);
 
   res.json(addToReadingList);
+});
+
+router.put("/:id", tokenExtractor, async (req, res) => {
+  const readingList = await ReadingList.findByPk(req.params.id);
+
+  if (readingList) {
+    readingList.read = req.body.read;
+    readingList.save();
+    res.json({ read: readingList.read });
+  } else {
+    res.status(404).end();
+  }
+
 });
 
 module.exports = router;
