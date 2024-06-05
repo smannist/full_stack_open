2c921fc36389dce85adcf1893892189ce1210861
Sami Männistö <sami.mannisto1@gmail.com>
2024-06-05 11:14:02 +0300

    E.x. 13.19 - reading lists via migrations

--- part_13/src/models/index.js
+++ part_13/src/models/index.js
@@ -1,8 +1,12 @@
 const Blog = require("./blog");
 const User = require("./user");
+const ReadingList = require("./readingList");
 
 User.hasMany(Blog);
 Blog.belongsTo(User);
+
+User.belongsToMany(Blog, { through: ReadingList, as: "read_blogs" });
+Blog.belongsToMany(User, { through: ReadingList, as: "users_read" });
 
 module.exports = {
   Blog,
---
+++ part_13/src/models/readingList.js
@@ -0,0 +1,44 @@
+const { Model, DataTypes } = require("sequelize");
+
+const { sequelize } = require("../utils/db");
+
+class ReadingList extends Model {}
+
+ReadingList.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    read: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+    blogId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "blogs",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: "readingList",
+  }
+);
+
+module.exports = ReadingList;
